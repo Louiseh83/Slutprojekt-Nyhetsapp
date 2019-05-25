6f7f701091fd8d7a3ca641353d60bef8bb040360
Louise Hjelstroem <Louiseh_83@hotmail.com>
2019-05-25 13:54:31 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+import './App.css';
+import Nyhetslista from './components/Nyhetslista'
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+class App extends Component {
+constructor(props) {
+super(props);
+this.state = {
+articles: []};
+}
+
+componentDidMount() {
+fetch("https://newsapi.org/v2/top-headlines?country=se&category=entertainment&apiKey=b61b0d0a09194ae591b9f3253b0ac8e7")
+.then(function (response) {
+if (response.status !==200) {
+throw Error( `status:${response.status}`);
+}
+//gör något med det som kom tillbaka
+return response.json()
+} ).then( jsondata => {
+//gör något med json-objektet
+this.setState({ articles: jsondata.articles })
+}).catch(error =>{
+this.setState({ 
+articles: [{
+urlToImage: "fejk.jpg",
+description: "Något gick fel. $(error.message)",
+}]
+});
+})
+}
+
+render() {
+  const {
+    user,
+    signOut,
+    signInWithGoogle,
+  } = this.props;
+return (
+<div className="App">
+<header className="App-header">
+  <div>
+  {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Sign in here</p>
+          }
+
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
+  </div>
+<h1>The latest News</h1>
+<p className="underrubrik">Här kan du läsa dagsfärska nyheter dygnet runt</p>
+</header>
+
+
+<Nyhetslista 
+minaArtiklar={this.state.articles} /><footer className="App-footer">
+Designed by: Louise Hjelström - 2019
+</footer>
+</div>
+
+
+);
+}
+}
+
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCH31urZc0AKH1qsg9HnoDSe5hW0991OzY",
+    authDomain: "my-first-project-547a5.firebaseapp.com",
+    databaseURL: "https://my-first-project-547a5.firebaseio.com",
+    projectId: "my-first-project-547a5",
+    storageBucket: "my-first-project-547a5.appspot.com",
+    messagingSenderId: "828001325758",
+    appId: "1:828001325758:web:ba39aeafb46176b5"
+  };
+
+  export default firebaseConfig;
