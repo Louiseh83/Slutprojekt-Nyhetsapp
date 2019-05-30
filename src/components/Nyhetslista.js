8a1638df0b0cca18b75fc71adc1b0f2f4b88d84a
Louise Hjelstroem <Louiseh_83@hotmail.com>
2019-05-30 23:24:25 +0200

    Har skrivit in lite kommentarer

--- src/components/Nyhetslista.js
+++ src/components/Nyhetslista.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; // Skrivs in för att appen ska fatta att React är en komponent som den ska använda.
 import Nyhetsartikel from './Nyhetsartikel';
 
 
@@ -7,13 +7,14 @@ Funktionen består av en samling satser och vad de ska göra. */
 function Nyhetslista(props) { 
    return (<section className="grid">
       {props.minaArtiklar.map(function (item, index) {
-         return <Nyhetsartikel // Man ber funktionen att bearbeta data och skicka tillbaka resultatet. 
+         return <Nyhetsartikel // Man ber funktionen att bearbeta datan från nyhetsartiklarna och skickar sedan tillbaka dessa. 
           minArtikel={item} 
           key={`artikel${index}`}
           />
       })}
-      </section>);
+      </section>); // Själva sektionen är en nyhetslista.
    }
 
 
-export default Nyhetslista;
+export default Nyhetslista; /* För att man ska kunna importera Nyhetslistan 
+till appen, som om det vore en vanlig HTML-tagg, så måste man exportera den så den går att komma åt */
